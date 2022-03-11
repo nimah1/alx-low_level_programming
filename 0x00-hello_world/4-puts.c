421cb68314d9ff6e588bda413752b4cd65d4da82
root <nojimudeegit config --global user.email nojimudee>
2022-03-11 10:31:07 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,8 @@
  *
  * Return - Always 0 (success)
  */
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	getchar();
+	puts("'Programming is like building a multilingual puzzle");
 	return (0);
 }
-	
-
