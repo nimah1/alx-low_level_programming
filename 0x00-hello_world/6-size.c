5112f69821b07ad7c969a5040abcb94fa8d7a878
root <nojimudeegit config --global user.email nojimudee>
2022-03-11 09:54:10 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
-	char chartype;
-	int integ;
-	long int longint;
-	long long int lonlongint;
+	char c;
+	int inte;
+	long int longt;
+	long long int llongt;
 	float floo;
 
-	printf("size of char: %d byte(s)\n",sizeof(chartype));
-	printf("size of int: %d byte(s)\n",sizeof(integ));
-	printf("size of long int: %ld byte(s)\n",sizeof(longint));
-	printf("size of long long int: %ld byte(s)\n",sizeof(lonlongint));
+
+	printf("size of char: %d byte(s)\n",sizeof(c));
+	printf("size of int: %d byte(s)\n",sizeof(inte));
+	printf("size of long int: %ld byte(s)\n",sizeof(longt));
+	printf("size of long long int: %ld byte(s)\n",sizeof(llongt));
 	printf("size of float: %d byte(s)\n",sizeof(floo));
 	return (0);
 }
