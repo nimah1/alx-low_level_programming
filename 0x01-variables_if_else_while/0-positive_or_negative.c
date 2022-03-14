58f23d3b0a3422071ee64913e7420760361733d2
root <nojimudeegit config --global user.email nojimudee>
2022-03-13 20:25:20 -0700

    positiveornegative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  *main - Entry point
  *
+ *Return: returns 0
  */
 int main(void)
 {
@@ -10,11 +12,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0){
-	printf("%d is positive\n", n);
-	}else if (n == 0){
-	printf("%d is zero\n" , n);
+	if (n > 0) {
+		printf("%d is positive\n", n);
+	}else if (n == 0) {
+		printf("%d is zero\n" , n);
 	}else {
-	printf("%d is negative\n" , n);
+		printf("%d is negative\n" , n);
 	}
+	return (0);
 }
