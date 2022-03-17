40921f512de59e35496e4f852ec45126b0739036
root <nojimudeegit config --global user.email nojimudee>
2022-03-17 13:48:18 -0700

    isalpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,6 +13,5 @@ void print_alphabet_x10(void)
 	{
 	 	_putchar(j);
 	}
-		_putchar(i);
-	}	
-}
+		_putchar('\n');
+	}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+##include "main.h"
+/**
+ *_isalpha - checks for alphabetic character
+ *
+ *Return: returns 1 if positive and 0 if otherwise
+ */
+int _isalpha(int c);
+{
+	int c;
+
+	_isalpha(c);
+	return (0);
+}
