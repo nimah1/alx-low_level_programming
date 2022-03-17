6a8960f29df1d0aa3af1ff20ca975326ea338a40
root <nojimudeegit config --global user.email nojimudee>
2022-03-17 13:21:34 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ *_islower - checks for lowercase
+ *
+ *Return: returns 1 if lowercase, returns 0 otherwise
+ */
+int _islower(int c)
+{
+	int c;
+
+	_islower(c);
+	return (0);
+}
