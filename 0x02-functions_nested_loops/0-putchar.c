02d41a478b44579b7d5d11e544cb6dd928474e71
root <nojimudeegit config --global user.email nojimudee>
2022-03-17 09:55:39 -0700

    alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,19 @@
  *
  *return: returns 0(success)
  */
-int _putchar(char c);
+int _putchar(char c)
 int _putchar(char c)
 {
-	
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
+	char c = "_putchar"
+
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	_putchar("\n");
 
 	return (0);
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void printalphabet(void)
+{
+	int i = "a";
+
+	while (i < "z")
+	{
+	_putchar(i);
+	_putchar("\n");
+	i++;
+	}
+}
