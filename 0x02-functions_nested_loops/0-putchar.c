f901897cac3ef18701793c65bbec84d6bcab3720
root <nojimudeegit config --global user.email nojimudee>
2022-03-17 10:37:11 -0700

    prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,10 @@
  *main - Entry point
  *
  *return: returns 0(success)
+ *
  */
-int _putchar(char c)
-int _putchar(char c)
+int main(void)
 {
-	char c = "_putchar"
-
 	_putchar("_");
 	_putchar("p");
 	_putchar("u");
