aab1123d03b8d05dc909290b32d11c0ee09c26e2
root <nojimudeegit config --global user.email nojimudee>
2022-03-17 20:12:14 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs - computes absolute value
+ *@c: parameter to be computed
+ *
+ *Return: always 0(success)
+ */
+int _abs(int c)
+{
+	int j;
+
+	while (c < 0)
+	{
+		j = (c / -1);
+	}
+	return (0);
+}
